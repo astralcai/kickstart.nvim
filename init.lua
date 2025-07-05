062fbae99e7e91aaa2b22aaedf88d58bf4db2eef
Astral Cai <astralcai@gmail.com>
2025-07-05 13:04:13 -0400

    Set default python host

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Python
+vim.g.python3_host_prog = '~/.pyenv/shims/python'
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
